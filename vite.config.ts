5524a69e9f21f7acfe6d51b818c827c50fe53564
Raspberry <gonzalo.bartual.fernandez2@gmail.com>
2026-01-19 16:17:44 +0100

    Fixes Vite.config in main

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,19 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/foodiepair/', 
+  server: {
+    host: true,
+    port: 5173,
+    allowedHosts: [
+      'bartualfdez.asuscomm.com'
+    ],
+    hmr: {
+      // Use your public domain for the WebSocket connection
+      host: 'bartualfdez.asuscomm.com',
+      // Force SSL for the websocket (needed for https sites)
+      protocol: 'wss', 
+      // We leave 'path' out because 'base' handles it automatically now
+    }
+  }
 })
